7c4f088dfee8e9157184c985bbe2753d2ffc1673
Tomoaki Masuda <masuda@moonmile.net>
2015-12-17 10:43:55 +0900

    CustomButton をちょっと修正

--- MStopWatch/MStopWatch/MStopWatch/CustomButton.cs
+++ MStopWatch/MStopWatch/MStopWatch/CustomButton.cs
@@ -31,38 +31,4 @@ namespace MStopWatch
             set { SetValue(ModeProperty, value); }
         }
     }
-#if false
-    class CustomButton : Button
-    {
-        /// <summary>
-        /// モードを指定
-        /// </summary>
-        /// 
-        public static readonly BindableProperty ModeProperty =
-            BindableProperty.Create<CustomButton, int>(
-                p =>
-                    p.Mode,
-                    0,
-                    propertyChanged: (bindable, oldValue, newValue) =>
-                    {
-                        var uc = bindable as CustomButton;
-                        /*
-                        switch ((int)newValue)
-                        {
-                            case 0: uc.Text = "開始"; break;
-                            case 1: uc.Text = "停止"; break;
-                            case 2: uc.Text = "リセット"; break;
-                        }
-                        */
-                    ((CustomButton)bindable).Mode = newValue;
-                    });
-
-        // 依存プロパティのラッパー
-        public int Mode
-        {
-            get { return (int)GetValue(ModeProperty); }
-            set { SetValue(ModeProperty, value); }
-        }
-    }
-#endif
 }
